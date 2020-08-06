648c94eed6055b2f8c3f0dd2a2062ecf9d85979c
preethi-ilango <55414451+preethi-ilango@users.noreply.github.com>
2020-08-06 13:51:11 -0400

    Update test_mac_fun.py

--- test_mac_fun.py
+++ test_mac_fun.py
@@ -9,7 +9,4 @@ FICR_BASE = 0x10000000
 def test_check():
  assert (main.mac2str(FICR_BASE)!=None)
 
-
-def test_call_quit_ends_the_game():                                                                      
- pytest_wrapped_exit = pytest.raises(SystemExit)                   
- assert main.check() == pytest_wrapped_exit.type 
+ 
