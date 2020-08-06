b359c3759c6a595f9442b82952407cfc1ec90707
preethi-ilango <55414451+preethi-ilango@users.noreply.github.com>
2020-08-06 13:50:24 -0400

    Update test_mac_fun.py

--- test_mac_fun.py
+++ test_mac_fun.py
@@ -10,7 +10,6 @@ def test_check():
  assert (main.mac2str(FICR_BASE)!=None)
 
 
-def test_call_quit_ends_the_game():                                       
-    """Tests call_quit and confirming ends the session."""                                 
+def test_call_quit_ends_the_game():                                                                      
  pytest_wrapped_exit = pytest.raises(SystemExit)                   
  assert main.check() == pytest_wrapped_exit.type 
